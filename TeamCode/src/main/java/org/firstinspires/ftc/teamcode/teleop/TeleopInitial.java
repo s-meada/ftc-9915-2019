c13d3cf958ee87d550bd6defeee6bba34c7ed159
trevorkw7 <trevorkw7@users.noreply.github.com>
2019-10-04 15:54:44 -0700

    Update TeleopInitial.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopInitial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleopInitial.java
@@ -9,8 +9,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import static android.animation.ValueAnimator.REVERSE;
 
-@TeleOp(name="TeleopInitial",group="Skystone")
+
+
+
+@TeleOp(name="TeleopInitial_C",group="Skystone")
 public class TeleopInitial extends OpMode {
+
+    public static final double ANGLE_MOTOR_COUNTS_PER_REV = 7168.0;
+    public static final double EXTENSION_MOTOR_COUNTS_PER_REV = 537.6;
 
     //declare motors
     DcMotor rightFront;
@@ -23,47 +29,63 @@ public class TeleopInitial extends OpMode {
     //declare servos
     Servo rotationServo;
     Servo grabberServo;
+    Servo verticalServo;
+
+    int currentPositionAngle = 0;
+    int currentPositionExtension = 0;
+    double verticalServoAngleFactor = 1.333/ANGLE_MOTOR_COUNTS_PER_REV;
+    double extensionMotorAngleFactor = EXTENSION_MOTOR_COUNTS_PER_REV/ANGLE_MOTOR_COUNTS_PER_REV;
+    boolean stoneTucked = false;
 
     @Override
     public void init() {
 
         //init driving motors
-        /*
+
         rightFront = hardwareMap.dcMotor.get("RightFront");
         leftFront = hardwareMap.dcMotor.get("LeftFront");
         rightBack = hardwareMap.dcMotor.get("RightBack");
         leftBack = hardwareMap.dcMotor.get("LeftBack");
-        */
 
-//        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-//        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //init motors
+
         angleMotor = hardwareMap.dcMotor.get("angleMotor");
+        angleMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         angleMotor.setTargetPosition(0);
+
         angleMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         angleMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        angleMotor.setPower(1);
 
 
         extensionMotor = hardwareMap.dcMotor.get("extensionMotor");
         extensionMotor.setTargetPosition(0);
         extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extensionMotor.setPower(1);
 
 
 
         //init servos
         rotationServo = hardwareMap.servo.get("rotationServo");
         grabberServo = hardwareMap.servo.get("grabberServo");
+        verticalServo = hardwareMap.servo.get("verticalServo");
 
-        rotationServo.setPosition(0);
-        grabberServo.setPosition(0);    //pos 0 = closed?
+
+        rotationServo.setPosition(0.5);
+        grabberServo.setPosition(0.5);    //pos 0 = closed?
+        verticalServo.setPosition(0.5);   //vertical with arm horizontal
+
     }
 
     @Override
     public void loop() {
 
-        mecanum drive
+        //mecanum drive
         double speed = -gamepad1.left_stick_y; //may or may not be reversed
         double strafe = -gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
@@ -74,22 +96,42 @@ public class TeleopInitial extends OpMode {
         rightBack.setPower(speed+strafe-turn);
         leftBack.setPower(speed-strafe+turn);
 
-        int currentPositionAngle = angleMotor.getCurrentPosition();
-        int positionChangeAngle = (int)(gamepad2.right_stick_y * 25);
-        angleMotor.setTargetPosition(currentPositionAngle + positionChangeAngle);
-        angleMotor.setPower(1);
+//
+        int positionChangeAngle = (int)(-gamepad2.right_stick_y * 6);
+        currentPositionAngle += positionChangeAngle;
+        if(currentPositionAngle > 1300) currentPositionAngle = 1300;
+        if(currentPositionAngle < 0) currentPositionAngle = 0;
+        telemetry.addData("Angle",currentPositionAngle);
 
-        int currentPositionExtension = extensionMotor.getCurrentPosition();
-        int positionChangeExtension = (int)((gamepad2.right_trigger - gamepad2.left_trigger) * 50);
-        extensionMotor.setTargetPosition(currentPositionExtension + positionChangeExtension);
-        extensionMotor.setPower(1);
+        angleMotor.setTargetPosition(currentPositionAngle);
+
+        //calculates the adjustment to the vertical-position-holding servo and
+        double verticalAngleOffset = (double)currentPositionAngle*verticalServoAngleFactor;
+        double verticalServoPosition = 0.5 + verticalAngleOffset;
+        if(stoneTucked) verticalServoPosition = 0.93;
+        verticalServo.setPosition(verticalServoPosition);
+
+
+        int extensionPositionOffset = (int)((double)currentPositionAngle*extensionMotorAngleFactor);
+
+
+//        int currentPositionExtension = extensionMotor.getCurrentPosition();
+        int positionChangeExtension = (int)((gamepad2.right_trigger - gamepad2.left_trigger) * 10);
+        currentPositionExtension += positionChangeExtension;
+        telemetry.addData("Extension ",currentPositionExtension);
+
+        if(currentPositionExtension > 1830 ) currentPositionExtension = 1830;
+        if(currentPositionExtension < 0) currentPositionExtension = 0;
+
+        extensionMotor.setTargetPosition(currentPositionExtension + extensionPositionOffset);
+
 
 
         if(gamepad2.a) { //test positions TBD
-            grabberServo.setPosition(0.9);
+            grabberServo.setPosition(1.0);
         }
         if(gamepad2.b) {
-            grabberServo.setPosition(0.3);
+            grabberServo.setPosition(0.5);
         }
 
         if(gamepad2.y){
@@ -97,12 +139,15 @@ public class TeleopInitial extends OpMode {
         }
 
         if(gamepad2.x){
-            rotationServo.setPosition(0); //y = 0; not tested
+            rotationServo.setPosition(0.17); //y = 0; not tested
         }
 
-        int currentPositionRotation = rotationServo.getCurrentPosition();
-        int positionChangeRotation = (int) (gamepad2.left_stick_x * 25);
+        if(gamepad2.dpad_up) stoneTucked = true;
+        if(gamepad2.dpad_down) stoneTucked = false;
 
+//        double currentPositionRotation = rotationServo.getPosition();
+//        int positionChangeRotation = (int) (gamepad2.left_stick_x * 50);
+        telemetry.update();
 
     }
 
