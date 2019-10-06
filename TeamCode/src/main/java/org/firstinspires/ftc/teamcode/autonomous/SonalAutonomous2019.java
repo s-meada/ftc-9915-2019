03bd1df7caf37de55fa82537c6d16cf97f813ae5
EthanProgramming <49596436+EthanProgramming@users.noreply.github.com>
2019-10-06 15:56:42 -0700

    Finish Sonal's autonomous code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SonalAutonomous2019.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SonalAutonomous2019.java
@@ -14,27 +14,36 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.common.Robot;
 
+import static org.firstinspires.ftc.teamcode.autonomous.AlignAndPickUpSkystone.STATE_END;
+import static org.firstinspires.ftc.teamcode.common.Robot.ANGLE_MOTOR_DOWN_LIMIT;
+import static org.firstinspires.ftc.teamcode.common.Robot.EXTENSION_MOTOR_EXTENDED_POSITION;
+import static org.firstinspires.ftc.teamcode.common.Robot.EXTENSION_MOTOR_RETRACTED_POSITION;
+import static org.firstinspires.ftc.teamcode.common.Robot.GRABBER_SERVO_OPEN_POSITION;
+
 public class SonalAutonomous2019 extends LinearOpMode {
     
     Robot robot = new Robot();
     int state;
-    //position variables
-    double extendedArmPosition = 1;
-    double loweredArmPosition = 2000;
-    double clawReleasingPosition = 1.0;
-    double retractedArmPosition = 2000;
-    double raisedArmPosition = 0;
+    //variables
+    double drivePower = -0.5;
+    double strafePower = -0.75;
+    double behindFoundationPosition = 32;
+    double towardsCenterPosition = 12;
+    double towardsRedLinePosition = 25;
+
+
 
     //cases
     static final int ROBOT_EXTENDS_ARM = 1;
-    static final int ROBOT_LOWERS_ARM = 2;
+    static final int ROBOT_RAISES_ARM = 2;
     static final int ROBOT_RELEASES_SKYSTONE = 3;
     static final int ROBOT_RETRACTS_ARM = 4;
-    static final int ROBOT_RAISES_ARM = 5;
+    static final int ROBOT_LOWERS_ARM = 5;
     static final int ROBOT_MOVES_BEHIND_FOUNDATION = 6;
     static final int ROBOT_STRAFES_CLOSER_TO_CENTER = 7;
     static final int ROBOT_MOVES_BACKWARDS = 8;
     static final int ROBOT_STOPS = 9;
+    static final int END_STATE = 10;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -48,9 +57,64 @@ public class SonalAutonomous2019 extends LinearOpMode {
             telemetry.addData("Current State", state);
             switch (state) {
                 case ROBOT_EXTENDS_ARM:
+                    robot.extensionMotor.setTargetPosition(EXTENSION_MOTOR_EXTENDED_POSITION);
+                    goToNextState();
+                    break;
+
+                case ROBOT_RAISES_ARM:
+                    robot.angleMotor.setTargetPosition(500);
+                    goToNextState();
+                    break;
+
+                case ROBOT_RELEASES_SKYSTONE:
+                    robot.grabberServo.setPosition(GRABBER_SERVO_OPEN_POSITION);
+                    goToNextState();
+                    break;
+
+                case ROBOT_RETRACTS_ARM:
+                    robot.extensionMotor.setTargetPosition(EXTENSION_MOTOR_RETRACTED_POSITION);
+                    goToNextState();
+                    break;
+
+                case ROBOT_LOWERS_ARM:
+                    robot.angleMotor.setTargetPosition(ANGLE_MOTOR_DOWN_LIMIT);
+                    goToNextState();
+                    break;
+
+                case ROBOT_MOVES_BEHIND_FOUNDATION:
+                    robot.drive(drivePower, behindFoundationPosition);
+                    goToNextState();
+                    break;
 
 
+                case ROBOT_STRAFES_CLOSER_TO_CENTER:
+                    robot.strafe(strafePower, towardsCenterPosition);
+                    goToNextState();
+                    break;
+
+
+                case ROBOT_MOVES_BACKWARDS:
+                    robot.drive(drivePower, towardsRedLinePosition);
+                    goToNextState();
+                    break;
+
+
+                case ROBOT_STOPS:
+                    robot.stop();
+                    goToNextState();
+                    break;
+
+
+                default:
+                    state = STATE_END;
+                    break;
             }
+
         }
     }
+
+    public void goToNextState() { state++; }
+    public void goToState(int newState) {
+        state = newState; }
+
 }
