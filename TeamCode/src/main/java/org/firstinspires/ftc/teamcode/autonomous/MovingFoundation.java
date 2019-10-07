3898bc125bc80a19b14423077dd052488fa936c6
Nightmerp <averyz@gmail.com>
2019-10-06 18:46:18 -0700

    Update MovingFoundation.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MovingFoundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MovingFoundation.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -8,20 +9,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.common.Robot;
 
+
+
+@Autonomous(name = "Moving Foundation", group = "test")
 public class MovingFoundation extends LinearOpMode {
 
     Robot robot = new Robot();
 
     int state = 1;
 
-    static final int DISTANCE_FROM_FRONT_WALL = 19; // inches
-    static final int DISTANCE_FROM_FOUNDATION = 25; // millimeters
-    static final int DISTANCE_FROM_SIDE_WALL = 2;  // inches
-
-    static final int DRIVE_TO_WALL = 1;
-    static final int DRIVE_TO_FOUNDATION = 2;
-    static final int DRAG_FOUNDATION = 3;
-    static final int STATE_END = 5;
+    static final int DRIVE_AWAY_FROM_BLOCK = 1;
+    static final int DRIVE_TO_WALL_1 = 2;
+    static final int DRIVE_TO_WALL_2 = 3;
+    static final int DRIVE_TO_WALL_3 = 4;
+    static final int DRIVE_TO_FOUNDATION = 5;
+    static final int DRAG_FOUNDATION = 6;
+    static final int STATE_END = 7;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -32,14 +35,39 @@ public class MovingFoundation extends LinearOpMode {
         while(opModeIsActive()) {
 
             telemetry.addData("Current State: ", state);
+            telemetry.addData("Distance in inches: ", robot.sideDistanceSensor.getDistance(DistanceUnit.INCH));
+            telemetry.update();
 
             switch(state) {
-                case DRIVE_TO_WALL:
+                case DRIVE_AWAY_FROM_BLOCK:
 
-                    robot.strafe(1.0, 4);
+                    if (robot.strafe(0.75, -2)) {
+                        robot.stop();
+                        goToNextState();
+                    }
+                    break;
+
+                case DRIVE_TO_WALL_1:
+
+                    if (robot.drive(0.5,55)) {
+                        robot.stop();
+                        goToNextState();
+                    }
+                    break;
+
+                case DRIVE_TO_WALL_2:
+
                     robot.setModeChassisMotors(DcMotor.RunMode.RUN_USING_ENCODER);
-                    robot.drivePower(1.0,1.0,1.0,1.0);
-                    if(robot.frontDistanceSensor.getDistance(DistanceUnit.INCH) < DISTANCE_FROM_FRONT_WALL){
+                    robot.drivePower(0.5,0.5,0.5,0.5);
+                    if (robot.sideDistanceSensor.getDistance(DistanceUnit.INCH) < 10) {
+                        robot.stop();
+                        goToNextState();
+                    }
+                    break;
+
+                case DRIVE_TO_WALL_3:
+                    robot.setModeChassisMotors(DcMotor.RunMode.RUN_TO_POSITION);
+                    if (robot.drive(0.5,12.0)) {
                         robot.stop();
                         goToNextState();
                     }
@@ -47,8 +75,9 @@ public class MovingFoundation extends LinearOpMode {
 
                 case DRIVE_TO_FOUNDATION:
 
-                    robot.drivePower(0.5,-0.5,-0.5,0.5);
-                    if(robot.foundationDistanceSensor.getDistance(DistanceUnit.MM) < DISTANCE_FROM_FOUNDATION){
+                    double distance = robot.sideDistanceSensor.getDistance(DistanceUnit.INCH) - 1.0;
+
+                    if (robot.strafe(0.75,distance)) {
                         robot.stop();
                         goToNextState();
                     }
@@ -57,9 +86,9 @@ public class MovingFoundation extends LinearOpMode {
                 case DRAG_FOUNDATION:
 
                     robot.foundationServo.setPosition(robot.FOUNDATION_SERVO_DOWN_POSITION);
-                    robot.drivePower(-1,1,1,-1);
-                    if(robot.sideDistanceSensor.getDistance(DistanceUnit.INCH) < DISTANCE_FROM_SIDE_WALL){
+                    if (robot.strafe(0.75, -34)) {
                         robot.stop();
+                        robot.foundationServo.setPosition(robot.FOUNDATION_SERVO_UP_POSITION);
                         goToNextState();
                     }
                     break;
