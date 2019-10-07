824bc6d8a65cba16d098d84cd7b351726cc42b2e
Ashley Mead <ashleyfromedmonds@gmail.com>
2019-10-06 18:33:04 -0700

    Use the VuforiaData class in AlignAndPickUpSkystone program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AlignAndPickUpSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AlignAndPickUpSkystone.java
@@ -22,10 +22,10 @@ public class AlignAndPickUpSkystone extends LinearOpMode {
 
     static final int FIND_CENTER_OF_SKYSTONE_VS_ARM = 1;
     static final int ADJUST_ROBOT_POSITION          = 2;
-    static final int MOVE_ARM_UP                    = 3;
-    static final int EXTEND_ARM                     = 4;
+//    static final int MOVE_ARM_UP                    = 3;
+//    static final int EXTEND_ARM                     = 4;
 
-    static final int STATE_END                      = 5; //EDIT as more states are added
+    static final int STATE_END                      = 3; //EDIT as more states are added
 
     //TODO: Add other states mentioned in pseudo code
 
@@ -92,17 +92,17 @@ public class AlignAndPickUpSkystone extends LinearOpMode {
                     }
                     break;
 
-                case MOVE_ARM_UP:
-                    if(robot.moveMotorToDistance(robot.angleMotor, robot.ANGLE_MOTOR_TICKS_PER_ROTATION, robot.ANGLE_GEARS_INCHES_PER_ROTATION,0.5, 10)) {
-                        goToNextState();
-                    }
-                    break;
-
-                case EXTEND_ARM:
-                    distanceForArmToExtend = robotXDistanceFromSkystoneCenter - robot.ARM_ORIGINAL_LENGTH_IN_FRONT_OF_ROBOT;
-                    robot.moveMotorToDistance(robot.extensionMotor, robot.EXTENSION_MOTOR_TICKS_PER_ROTATION,robot.EXTENSION_SPROCKETS_INCHES_PER_ROTATION, 0.5, (int)(distanceForArmToExtend));
-                    goToNextState();
-                    break;
+//                case MOVE_ARM_UP:
+//                    if(robot.moveMotorToDistance(robot.angleMotor, robot.ANGLE_MOTOR_TICKS_PER_ROTATION, robot.ANGLE_GEARS_INCHES_PER_ROTATION,0.5, 10)) {
+//                        goToNextState();
+//                    }
+//                    break;
+//
+//                case EXTEND_ARM:
+//                    distanceForArmToExtend = robotXDistanceFromSkystoneCenter - robot.ARM_ORIGINAL_LENGTH_IN_FRONT_OF_ROBOT;
+//                    robot.moveMotorToDistance(robot.extensionMotor, robot.EXTENSION_MOTOR_TICKS_PER_ROTATION,robot.EXTENSION_SPROCKETS_INCHES_PER_ROTATION, 0.5, (int)(distanceForArmToExtend));
+//                    goToNextState();
+//                    break;
 
                 default:
                     state = STATE_END;
