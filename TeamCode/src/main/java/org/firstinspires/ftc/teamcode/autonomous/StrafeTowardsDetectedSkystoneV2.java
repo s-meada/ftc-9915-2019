1f900701af75e9b4aab8e6ec647849def2c2f718
trevorkw7 <trevorkw7@users.noreply.github.com>
2019-10-11 17:57:10 -0700

    Structural Cahnges to V2 and Incorperated Vuforia into V1
    Encorperated new Vuforia Data class into StrafeToSkystone instead of StrafeToSkystoneV2 because V2 depends on ability for class to determine quarry configuration which is unable to be done for now. Instead, the vurofria class was encorperated into StrafeToSkystone which has a structure that can better match the capabilities of the vuforia class for now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StrafeTowardsDetectedSkystoneV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StrafeTowardsDetectedSkystoneV2.java
@@ -6,13 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.teamcode.ETC2019OpModes.NavigationWebcamVuforia;
 import org.firstinspires.ftc.teamcode.common.Robot;
-@Autonomous(name = "StrafeSkystone", group = "auto")
+@Autonomous(name = "StrafeSkystoneV2", group = "auto")
 public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
 
     // Global variables go before runOpMode()
     Robot robot = new Robot();
 
-    NavigationWebcamVuforia vuforia = new NavigationWebcamVuforia();
+
+
 
     int state = 1;
 
@@ -22,9 +23,10 @@ public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
     static final int STRAFE_TO_SKYSTONE = 3;
     static final int STATE_END = 5;
 
+    int skyStonePosition = 1;
     double strafePower = 0.75;
     double drivePower = 0.5;
-    double viewingPosition = 11.1; //strafe
+    double viewingPosition = 15.1; //strafe
 
 
 
@@ -32,9 +34,10 @@ public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // init()
         robot.initForRunToPosition(hardwareMap);
-
+        SkystoneVuforiaData vision = new SkystoneVuforiaData(hardwareMap,robot);
         waitForStart(); // MUST add this yourself
 
+        vision.targetsSkyStone.activate();
         while(opModeIsActive()) {  // MUST add this yourself
             // loop()
             telemetry.addData("Current State", state);
@@ -44,18 +47,19 @@ public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
                     robot.strafe(strafePower, viewingPosition);
 
                     if(robot.strafe(strafePower, viewingPosition)); {
-                    goToNextState();
-                }
-                break;
+                        goToNextState();
+                        break;
+                    }
+
 
                 case DETERMINE_SKYSTONE:
                     // run vuforia to determine vuforia.skyStoneConfiguration()
-
+                    break;
 
 
                 case STRAFE_TO_SKYSTONE:
-                    telemetry.addData("Determined SkyStone Position:", vuforia.skyStonePosition);
-                    if (vuforia.skyStonePosition == 1){
+                    telemetry.addData("Determined SkyStone Position:", skyStonePosition);
+                    if (skyStonePosition == 1){
 
                         robot.strafe(strafePower,15);
 
@@ -68,19 +72,17 @@ public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
                     }
 
 
-                    if (vuforia.skyStonePosition == 2){
+                    if (skyStonePosition == 2){
 
                         robot.strafe(strafePower,15);
 
                         if(robot.strafe(strafePower,15)) {
 
-                            robot.drive(drivePower,1.0);
+                            robot.drive(drivePower, 1.0);
                         }
-
-
                     }
 
-                    if(vuforia.skystonePosition == 3){
+                    if(skyStonePosition == 3){
 
                         robot.strafe(strafePower,15);
 
@@ -88,10 +90,8 @@ public class StrafeTowardsDetectedSkystoneV2 extends LinearOpMode {
 
                             robot.drive(drivePower, 9.25);
                         }
-
                     }
 
-                    if()
                     break;
 
                 default:
