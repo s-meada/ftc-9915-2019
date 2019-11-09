8d9a467fba1478d94647c04708d2efaa58b2a5e5
trevorkw7 <trevorkw7@users.noreply.github.com>
2019-11-08 18:20:20 -0800

    Add TestSkystoneVuforiaData

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestSkystoneVuforiaData.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.common.Robot;
+
+import java.util.HashMap;
+
+@Autonomous(name = "Test SkystoneVuforiaData", group = "test")
+public class TestSkystoneVuforiaData extends LinearOpMode {
+    Robot robot = new Robot();
+    SkystoneVuforiaData vision;
+    boolean activated = false;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot.initRegular(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (!activated) {
+                // If this is in init() in iterative OpMode, the program will say stuck in init() and restart the app before vuforia can finish initializing.
+                // This may be part of the crashing problem.
+                vision = new SkystoneVuforiaData(hardwareMap, robot);
+                vision.targetsSkyStone.activate();
+                activated = true;
+            }
+            // The getSkystoneCoordinates() method returns null if the skystone is not detected
+            HashMap<String, Double> skyStoneCoordinates = vision.getSkystoneCoordinates();
+            if (skyStoneCoordinates != null) {
+                telemetry.addData("Skystone Pos (in)", "(X, Y) = %.1f, %.1f",
+                        skyStoneCoordinates.get("X"), skyStoneCoordinates.get("Y Modified"));
+                robot.light.setPower(0);
+            } else {
+                telemetry.addLine("No Skystone Detected");
+            }
+            telemetry.update();
+        }
+    }
+}
